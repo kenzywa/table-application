6081a2fcecb6edecc908738f787fafb77698b0ea
Roman <shukailo99@gmail.com>
2021-04-06 01:00:26 +0300

    Added to new folder "Employee"

---
+++ TableApplication/Employee/Employee.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct Employee {
+    var name: String
+    var surname: String
+    var age: Int
+    var profession: String
+    var male: String
+    var numberPhone: Int
+    let id : String
+}
