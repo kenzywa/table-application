01653f21fca43289d56fd098e30aad577c64c4dc
Roman <shukailo99@gmail.com>
2021-04-06 01:03:09 +0300

    no message

--- TableApplication/EmployeeEditorDelegate.swift
+++
@@ -1,8 +0,0 @@
-protocol EmployeeEditorDelegate {
-    func toSaveData(name : String,
-                    surname: String,
-                    profession : String,
-                    numberPhone : String,
-                    id : String)
-    func showAlertFailed()
-}
